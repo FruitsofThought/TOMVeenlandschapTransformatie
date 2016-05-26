73ccf4df02a3ea7f8678aa87f54d1c340008881a
Reinier Battenberg <reinier@mountbatten.nl>
2016-05-26 09:33:12 +0200

    Initial Commit

---
+++ TOMVeenlandschapTransformatie.js
@@ -0,0 +1,29 @@
+"use strict";
+define(
+  ['require', 'scene'],
+  function(require, Scene) {
+
+    class TOMVeenlandschapTransformatie extends Scene {
+      constructor() {
+          console.log("Going to Super Scene TOMVeenlandschapTransformatie");
+          var config = {
+            "id": "TOMVeenlandschapTransformatie",
+            "groupid": "landscape",
+            "languages": ['en-US', 'nl-NL'],
+          };
+          super(config);
+        }
+        // You must copy this, else we do not know where we are
+      get path() {
+        return require.toUrl("./").split('?')[0];
+      }
+      get legend() {
+        var legend = [];
+        legend['items'] = [];
+        return legend;
+      };
+    };
+
+    return new TOMVeenlandschapTransformatie();
+
+  });
